82bb1561f156471edbab5354c37abbc23fec65c6
wathika-eng <wathika02@gmail.com>
2024-06-03 18:48:19 +0300

    chore: Refactor MyString class to handle value assignment and sentence checking

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        print("Setting..")
+        return self.__value
+
+    @value.setter
+    def value(self, userin):
+        if isinstance(userin, str):
+            self.__value = userin
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        if self.__value.endswith("."):
+            return True
+        else:
+            return False
+
+
+# value = MyString()
+# value.value = 123
